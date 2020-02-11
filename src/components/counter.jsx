f3d811138ac42fdaff05e26ac32cff2c5da7200c
M.O Akindayo <matthewakinse@gmail.com>
2020-02-11 03:33:10 +0200

    Update counter component - Addition button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,31 @@ class Counter extends Component {
     count: 0
   };
 
+  // constructor() {
+  //  super();
+  //   this.handleAddition = this.handleAddition.bind(this);
+  // }
+
+  handleAddition = () => {
+    //In the strict mode, the 'this' is not available (undefined) except we bind as commented out above or use an arrow function such this one.
+    this.setState({ count: this.state.count + 1 });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.countZero()}</span>
-        <button>Add</button>
+        <span className={this.getClassBadge()}>{this.countZero()}</span>
+        <button onClick={this.handleAddition}>Add</button>
       </React.Fragment>
     );
   }
+  getClassBadge() {
+    // method for rendering dynamic badge class
+    let classBadge = "badge m-2 badge-";
+    classBadge += this.state.count === 0 ? "warning" : "primary";
+    return classBadge;
+  }
+
   countZero() {
     // using object destructuring - set count constant to this.state
     const { count } = this.state;
