4a914ffc3f587706c918e873ba8fee29a0c9ec1b
M.O Akindayo <matthewakinse@gmail.com>
2020-10-17 17:12:38 +0300

    Add links to the navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 class NavBar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Bookshop
-        </a>
+      // <nav className="navbar navbar-light bg-light">
+      //   <a className="navbar-brand" href="/">
+      //     Bookshop
+      //   </a>
+      // </nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Libstore
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link" to="books">Books</NavLink>
+            <NavLink className="nav-link" to="/readers">Readers</NavLink>
+            <NavLink className="nav-link" to="/borrowals">Loans</NavLink>
+          </div>
+        </div>
       </nav>
     );
   }
