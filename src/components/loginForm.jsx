f9f3cf3c2a45e50d3ef0f31981f6fc21794747a3
M.O Akindayo <matthewakinse@gmail.com>
2020-10-20 15:34:14 +0300

    Use Joi for input validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from './common/input';
+import Joi from 'joi-browser';
 
 class LoginForm extends Component {
   state = {
@@ -10,17 +11,26 @@ class LoginForm extends Component {
     errors: {}
   }
 
+
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
+  };
+
   validate = () => {
+
+    const { error } = Joi.validate(this.state.account, this.schema, { abortEarly: false });
+    // console.log(result)
+
+    if (!error) return null;
+
     const errors = {};
 
-    const { account } = this.state;
+    for (let item of error.details)
+      errors[item.path[0]] = item.message;
 
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.';
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.';
+    return errors
 
-    return Object.keys(errors).length === 0 ? {} : errors;
   };
 
   handleSubmit = e => {
@@ -36,12 +46,10 @@ class LoginForm extends Component {
   }
 
   validateProperty = ({ name, value }) => {
-    if (name === 'username') {
-      if (value.trim() === '') return 'Username is required.'
-    }
-    if (name === 'password') {
-      if (value.trim() === '') return 'Password is required.'
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] }
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   }
 
   handleChange = ({ currentTarget: input }) => {
@@ -67,7 +75,7 @@ class LoginForm extends Component {
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <Input name="username" value={account.username} label="Username" onChange={this.handleChange} error={errors.username} />
-          <Input name="password" value={account.password} label="password" onChange={this.handleChange} error={errors.password} />
+          <Input name="password" value={account.password} label="Password" onChange={this.handleChange} error={errors.password} />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
