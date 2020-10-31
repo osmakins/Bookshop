3e85f4c067401dcb2edc46597fee2dba7b6400b6
M.O Akindayo <matthewakinse@gmail.com>
2020-10-31 20:16:07 +0200

    Login method post to backend

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Joi from 'joi-browser';
-import Form from './common/form'
+import Form from './common/form';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
   state = {
@@ -11,22 +12,29 @@ class LoginForm extends Form {
     errors: {}
   }
 
-
   schema = {
     username: Joi.string().required().label('Username'),
     password: Joi.string().required().label('Password')
   };
 
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt)
+      this.props.history.push('/');
+    } catch (ex) {
+
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors })
+      }
+
+    }
 
 
-
-
-  doSubmit = () => {
-    console.log('submitted!')
   }
-
-
-
 
   // componentDidMount() {
   //   this.username.current.focus();
