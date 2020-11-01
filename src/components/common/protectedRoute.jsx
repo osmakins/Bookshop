7ec77af23eb2923b6e8d76c4bf26a7cf7ae17034
M.O Akindayo <matthewakinse@gmail.com>
2020-11-01 12:53:16 +0200

    Fix redirecting after login

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
   return (
     <Route {...rest} render={props => {
-      if (!auth.getCurrentUser()) return <Redirect to="/login" />;
+      if (!auth.getCurrentUser()) return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />;
       return Component ? <Component {...props} /> : render(props)
     }} />
   );
