a15e0f45c77b08b969c09216bcc8d92914fd8cb7
M.O Akindayo <matthewakinse@gmail.com>
2020-11-01 09:09:19 +0200

    Add jwt, getUser and Logout method

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import React from 'react';
-import NavBar from './components/navbar'
-import Books from "./components/books";
+import React, { Component } from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from './components/navbar'
+import jwtDecode from 'jwt-decode';
 import { ToastContainer } from 'react-toastify';
+import Books from "./components/books";
 import Readers from './components/readers';
 import Borrowals from './components/borrowals';
 import NotFound from './components/notFound';
 import BookForm from './components/bookForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
+import Logout from './components/logout';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      <NavBar />
-      <div className="jumbotron"></div>
-      <main className="container">
-        <Switch>
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/books/:id" component={BookForm} />
-          <Route path="/books" component={Books}></Route>
-          <Route path="/readers" component={Readers}></Route>
-          <Route path="/borrowals" component={Borrowals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/books" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+
+
+
+class App extends Component {
+
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user })
+    } catch (ex) {
+
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
+        <div className="jumbotron"></div>
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/books/:id" component={BookForm} />
+            <Route path="/books" component={Books}></Route>
+            <Route path="/readers" component={Readers}></Route>
+            <Route path="/borrowals" component={Borrowals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/books" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-class NavBar extends Component {
+const NavBar = ({ user }) => {
 
-  render() {
-    return (
-      // <nav className="navbar navbar-light bg-light">
-      //   <a className="navbar-brand" href="/">
-      //     Bookshop
-      //   </a>
-      // </nav>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          Libstore
+  return (
+    // <nav className="navbar navbar-light bg-light">
+    //   <a className="navbar-brand" href="/">
+    //     Bookshop
+    //   </a>
+    // </nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Libstore
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <NavLink className="nav-link" to="books">Books</NavLink>
-            <NavLink className="nav-link" to="/readers">Readers</NavLink>
-            <NavLink className="nav-link" to="/borrowals">Loans</NavLink>
-            <NavLink className="nav-link" to="/login">Login</NavLink>
-            <NavLink className="nav-link" to="/register">Register</NavLink>
-          </div>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="books">Books</NavLink>
+          <NavLink className="nav-item nav-link" to="/readers">Readers</NavLink>
+          <NavLink className="nav-item nav-link" to="/borrowals">Loans</NavLink>
+          {!user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
+              <NavLink className="nav-item nav-link" to="/register">Register</NavLink>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="/profile">{user.name}</NavLink>
+              <NavLink className="nav-item nav-link" to="/logout">Logout</NavLink>
+            </React.Fragment>
+          )}
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
+
 
 export default NavBar;
